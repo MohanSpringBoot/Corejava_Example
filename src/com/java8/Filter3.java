289a2a70776636b92946a6a81aa21e28e31f20dd
MohanSpringBoot <mohank.srk@gmail.com>
2023-11-15 23:40:20 +0530

    modified-233

--- src/com/java8/Filter3.java
+++ src/com/java8/Filter3.java
@@ -4,10 +4,10 @@ public class Filter3 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("mohanchanges");
-		System.out.println("mohanchanges");
-		System.out.println("mohanchanges");
-		System.out.println("mohanchanges");
+		System.out.println("mohanchanges1");
+		System.out.println("mohanchanges2");
+		System.out.println("mohanchanges3");
+		System.out.println("mohanchanges4");
 		
 
 	}
